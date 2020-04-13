6aa49253cc16d5a01c403c288824043e33f9ee8d
sara ebadi <ebadi.sara227@gmail.com>
2020-04-14 00:41:39 +0430

    Fix: http config

--- app/src/main/java/com/moviefinder/android/utils/Constants.kt
+++ app/src/main/java/com/moviefinder/android/utils/Constants.kt
@@ -2,7 +2,7 @@ package com.moviefinder.android.utils
 
 class Constants {
     companion object{
-        const val BASE_URL = "http://api.themoviedb.org/3/"
+        const val BASE_URL = "https://api.themoviedb.org/3/"
         const val BASE_URL_IMAGE= "https://image.tmdb.org/t/p/original"
         const val API_KEY = "2f43fbd58c7f64173269780e36ad4982"
         const val DOLLAR_UNIT = "2f43fbd58c7f64173269780e36ad4982"
