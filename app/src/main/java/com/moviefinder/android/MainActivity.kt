630ef39438fba2b02fc3b5f30a2e2c0840c7adbb
sara ebadi <ebadi.sara227@gmail.com>
2020-04-13 23:36:45 +0430

    Fix: class refactoring

--- app/src/main/java/com/moviefinder/android/MainActivity.kt
+++ app/src/main/java/com/moviefinder/android/MainActivity.kt
@@ -9,44 +9,5 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-
-        //        getSupportFragmentManager().beginTransaction().replace(R.id.container,MainFragment.newInstance()).commitNow();
-
-
-        //        NavHostFragment.findNavController(MainFragment.newInstance());
-
-        //        navController = Navigation.findNavController(view);
-
     }
-
-
-    //    public void fetchData() {
-    //        Call<MovieModel> request = generateRetrofit.apiClient().getSearchMovie(Constant.API_KEY, edtMovieSearch.getText().toString());
-    //        request.enqueue(new Callback<MovieModel>() {
-    //            @Override
-    //            public void onResponse(Call<MovieModel> call, final Response<MovieModel> response) {
-    //                if (response.isSuccessful()) {
-    //                    runOnUiThread(new Runnable() {
-    //                        @Override
-    //                        public void run() {
-    //                            resultSearchList = response.body().getResultSearchList();
-    //                            movieListAdapter.submitList(resultSearchList);
-    //                        }
-    //                    });
-    //                }
-    //            }
-    //
-    //            @Override
-    //            public void onFailure(Call<MovieModel> call, Throwable t) {
-    //                Log.i("onFailure", "run: " + t.getMessage());
-    //            }
-    //        });
-    //    }
-    //
-    //
-    //
-
-
-
 }
